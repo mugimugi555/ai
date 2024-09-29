384c0db23b1b9bc124966344acd4bc682b18431f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-09-29 10:54:24 +0900

    Create upscale.sh

---
+++ upscale/realesrgan/upscale.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# コマンドライン引数から入力ファイルを取得
+if [ "$#" -lt 1 ]; then
+    echo "使用法: ./upscale.sh <input_video_file>"
+    exit 1
+fi
+
+target_movie_file_name="$1"
+
+# 動画ファイル名から拡張子を取り除いて作業用ディレクトリ名を設定
+base_name=$(basename "$target_movie_file_name" .mp4)
+working_dir_target="${base_name}_target"
+working_dir_result="${base_name}_upscaled"
+
+# 出力ファイル名を指定 (元のファイル名に -upscaled を追加)
+output_movie_file_name="${base_name}-upscaled.mp4"
+
+# 作業用ディレクトリの作成と初期化
+mkdir -p "$working_dir_target"
+rm -f "$working_dir_target/*.png"
+
+mkdir -p "$working_dir_result"
+rm -f "$working_dir_result/*.png"
+
+# input.mp4からフレームをPNG形式で抽出
+ffmpeg -i "$target_movie_file_name" -vcodec png "$working_dir_target/%03d.png" > /dev/null 2>&1
+
+# PNGファイルの枚数をカウント
+totalFiles=$(find "$working_dir_target" -type f -name '*.png' | wc -l)
+
+# 対象画像ファイル数を表示
+echo "対象画像ファイル数: $totalFiles"
+
+# 入力ファイルのフレームレートを取得
+frameRate=$(ffmpeg -i "$target_movie_file_name" 2>&1 | grep -oP '\d+(\.\d+)? fps' | awk '{print $1}')
+
+# 入力フレームレートを表示
+echo "入力フレームレート: $frameRate"
+
+# Real-ESRGANで画像のアップスケールを実行し、進捗を表示
+processedFiles=0
+startTime=$(date +%s)
+
+# PNGファイルを取得
+files=("$working_dir_target"/*.png)
+
+for file in "${files[@]}"; do
+    outputFile="$working_dir_result/$(basename "$file")"
+
+    # Real-ESRGANで1枚ずつアップスケール
+    CMD="./realesrgan-ncnn-vulkan -i \"$file\" -o \"$outputFile\" -n realesrgan-x4plus -s 4 -f png"
+    eval "$CMD" > /dev/null 2>&1
+
+    processedFiles=$((processedFiles + 1))
+
+    # プログレスバーの表示を更新
+    progress=$((processedFiles * 100 / totalFiles))
+    echo -ne "\r進行状況: $progress%"
+
+    # 残り時間の推定
+    elapsedTime=$(( $(date +%s) - startTime ))
+    if [ "$processedFiles" -ne 0 ]; then
+        remainingTime=$(( (elapsedTime * (totalFiles - processedFiles)) / processedFiles ))
+    else
+        remainingTime=0
+    fi
+    remainingMinutes=$(( remainingTime / 60 ))
+    remainingSeconds=$(( remainingTime % 60 ))
+    echo -ne " 残り時間: $remainingMinutes分$remainingSeconds秒"
+done
+
+echo -e "\n"
+
+# 生成されたPNGから動画を作成（音声なし）の高画質設定
+ffmpeg -y -framerate "$frameRate" -i "$working_dir_result/%03d.png" -vf scale=3840:2160 -c:v hevc_nvenc -preset p7 -rc vbr -cq 17 -b:v 20M -maxrate 30M -bufsize 40M -pix_fmt yuv444p working_upscaled_none_audio.mp4 > /dev/null 2>&1
+
+# input.mp4から音声を抽出して、指定された出力ファイルに追加する
+ffmpeg -i working_upscaled_none_audio.mp4 -i "$target_movie_file_name" -c copy -map 0:v:0 -map 1:a:0 -shortest -y "$output_movie_file_name" > /dev/null 2>&1
+
+# 一時ファイルの削除
+rm -f working_upscaled_none_audio.mp4
